ce98b954e3e6263ac073d124284ac3f236aee3e1
Nick-Lisi <nlisi1@yahoo.com>
2020-01-02 17:44:53 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname));
 app.use(express.json());
 
 //app.use(express.static('public'));
